5dc466f5afe485c5df755531e4a41dc440d9d631
Khang Nguyen-Le <nlhuykhang@gmail.com>
2016-12-10 23:09:45 +0700

    Add UMD build version

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -1,5 +1,10 @@
 echo "> Start transpiling ES2015"
 echo ""
-./node_modules/.bin/babel --plugins "transform-runtime" src --ignore __tests__ --out-dir ./dist
+./node_modules/.bin/babel --plugins "transform-runtime" src --ignore __tests__  --out-dir ./dist/common
 echo ""
 echo "> Complete transpiling ES2015"
+echo ""
+echo "> Start building UMD"
+browserify index.js --standalone StdReactiveVar -o dist/umd/reactive-var-std.js
+uglifyjs dist/umd/reactive-var-std.js -o dist/umd/reactive-var-std.min.js
+echo "> Complete building UMD"
