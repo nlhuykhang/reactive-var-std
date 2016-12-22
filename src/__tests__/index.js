68b6e14fe4b1905fde04e77bbbc69895c4f27192
Khang Nguyen-Le <nlhuykhang@gmail.com>
2016-12-22 17:02:14 +0700

    Add unit test for StdReactiveVar constructor, _isEqual, .get, .getOld

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,10 +1,90 @@
-// import {expect} from 'chai';
-// import {sum} from '../';
-// const {describe, it} = global;
-//
-// describe('sum', () => {
-//   it('should add two numbers correctly', async () => {
-//     const result = await sum(10, 20);
-//     expect(result).to.be.equal(30);
-//   });
-// });
+import { expect } from 'chai';
+const { describe, it } = global;
+
+import StdReactiveVar from '../std-reactive-var.js';
+
+import Tracker from 'trackr';
+
+describe('reactive var only', function() {
+  it('should construct the object well', function() {
+    const target = new StdReactiveVar(10);
+
+    expect(target._oldValue).to.be.null;
+    expect(target._equalFunc).to.be.undefined;
+    expect(target._dep).to.be.an.instanceof(Tracker.Dependency);
+
+    expect(target._curValue).to.be.equal(10);
+
+    expect(target._isEqual).to.be.function;
+    expect(target.get).to.be.function;
+    expect(target.getOld).to.be.function;
+    expect(target.getNonReactive).to.be.function;
+    expect(target.set).to.be.function;
+    expect(target.toString).to.be.function;
+    expect(target._numListeners).to.be.function;
+  });
+
+  it('_isEqual should work with number', function() {
+    const target = new StdReactiveVar(10);
+
+    expect(target._isEqual(10)).to.be.true;
+    expect(target._isEqual('10')).to.be.false;
+    expect(target._isEqual(11)).to.be.false;
+  });
+
+  it('_isEqual should work with object', function() {
+    const obj = {};
+    const target = new StdReactiveVar(obj);
+
+    expect(target._isEqual(obj)).to.be.false;
+    expect(target._isEqual({})).to.be.false;
+    expect(target._isEqual(11)).to.be.false;
+  });
+
+  it('get should work with number', function() {
+    const target = new StdReactiveVar(10);
+    expect(target.get()).to.be.equal(10);
+  });
+
+  it('get should work with string', function() {
+    const target = new StdReactiveVar('10');
+    expect(target.get()).to.be.equal('10');
+  });
+
+  it('get should work with object', function() {
+    const obj = {};
+    const target = new StdReactiveVar(obj);
+    expect(target.get()).to.be.equal(obj);
+  });
+
+  it('get should work with null', function() {
+    const target = new StdReactiveVar(null);
+    expect(target.get()).to.be.null;
+  });
+
+  it('get should work with undefined', function() {
+    const target = new StdReactiveVar(undefined);
+    expect(target.get()).to.be.undefined;
+  });
+
+  it('getOld should work', function() {
+    const target = new StdReactiveVar(undefined);
+
+    // XXX not really sure I doing it right here? is using .set to test .getOld is the right way?
+    target.set(1);
+    expect(target.getOld()).to.be.undefined;
+
+    target.set('1');
+    expect(target.getOld()).to.be.equal(1);
+
+    target.set(null);
+    expect(target.getOld()).to.be.equal('1');
+
+    target.set(undefined);
+    expect(target.getOld()).to.be.null;
+  });
+});
+
+describe('reactive var with tracker', function() {
+  it('', function() {});
+});
